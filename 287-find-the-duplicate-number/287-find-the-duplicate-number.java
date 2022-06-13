e0070c27dbf71dee03c003722b982e7e47a34cce
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-13 20:30:44 +0530

    Time: 5 ms (80.58%), Space: 76.8 MB (17.97%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -4,7 +4,7 @@ class Solution {
         int n = nums.length;
         int freq[] = new int[n];
         
-        System.out.println(Arrays.toString(freq));
+        // System.out.println(Arrays.toString(freq));
         
         for(int i=0; i<nums.length; i++){
             if(freq[nums[i]] == 0){
