28270724aeeafa7ae6ff91496a6b485152e29220
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-15 02:42:23 +0530

    Time: 2 ms (73.64%), Space: 54.8 MB (47.05%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int low = -1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0){
+                low = i;
+                break;
+            }
+        }
+        // System.out.println(nums[low]+" "+low);
+        int high = low+1;
+        while(low>=0 && high<nums.length){
+            if(nums[low]==0 && nums[high]==0){
+                high++;
+                continue;
+            }
+            swap(low, high, nums);
+            low++;
+            high++;
+        }
+            
+        
+    }
+    
+    
+    private void swap(int x, int y, int[] nums){
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}
