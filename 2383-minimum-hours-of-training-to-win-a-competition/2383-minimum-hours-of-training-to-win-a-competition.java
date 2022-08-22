0b2a2d7b2c542fa2d0d5ec86f271aeb4548b568d
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-23 01:18:53 +0530

    Time: 9 ms (28.57%), Space: 42.6 MB (14.29%) - LeetHub

--- 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.java
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.java
@@ -2,36 +2,43 @@ class Solution {
     public int minNumberOfHours(int ie, int ix, int[] energy, int[] experience) {
      
         
-        int ene = 0;
+        int E = 0;
         int eneReq = 0;
         for(int i : energy){
             eneReq += i;
         }
         if(ie > eneReq){
-            ene = 0;
+            E = 0;
         }
         else{
-            ene = eneReq-ie+1;
+            E = eneReq-ie+1;
         }
         
         
-        int exp = 0;
+        int X = 0;
         for(int i : experience){
             if(ix > i){
                 ix += i;
             }
             else{
-                // int x = i+1;
-                exp += i-ix+1;
-                ix += exp+i;
+                X += (i-ix)+1;
+                ix += X+i;
             }
         }
-        // System.out.println(E+" "+X);
+        System.out.println(E+" "+X);
         
-       return ene+exp;
+       return E+X;
     }
 }
-  
+
+// 94 [58,47,100,71,47,6,92,82,35,16,50,15,42,5,2,45,22]
+
+
+// 70
+// [77,83,99,76,75,66,58,84,44,98,70,41,48,7,10,61,28]
+    // i
+
+
 
 
 
