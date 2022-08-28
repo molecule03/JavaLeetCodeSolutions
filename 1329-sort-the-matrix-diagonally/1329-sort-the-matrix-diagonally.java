8a17fd4c39f29efde205108c5780949aad6aae2a
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-28 20:20:54 +0530

    Time: 12 ms (47.83%), Space: 48 MB (50.43%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
         
-        List<List<Integer>> sorted = new ArrayList<>();
-        
         int n = mat.length;
         int m = mat[0].length;
         
         for(int i=m-1; i>=0; i--){
+            
             int sr = 0;
             int sc = i;
             List<Integer> list = new ArrayList<>();
@@ -15,24 +14,20 @@ class Solution {
                 sc++;
                 sr++;
             }
-            Collections.sort(list);
-            // System.out.println(list);
+            
             sr = 0;
             sc = i;
+            Collections.sort(list);
             while(sc < m && sr < n ){
                 mat[sr][sc] = list.get(sr);
                 sc++;
                 sr++;
             }
             
-            
-            
-            
-            
-            sorted.add(list);
         }
         
         for(int i=1; i<n; i++){
+            
             int sr = i;
             int sc = 0;
             List<Integer> list = new ArrayList<>();
@@ -41,17 +36,16 @@ class Solution {
                 sc++;
                 sr++;
             }
-            Collections.sort(list);
-            // System.out.println(list);
-            
+    
             sr = i;
             sc = 0;
+            Collections.sort(list);
             while(sc < m && sr < n ){
                 mat[sr][sc] = list.get(sc);
                 sc++;
                 sr++;
             }
-            sorted.add(list);
+            
         }
         
         return mat;
